e82f072677c55743e196810f58d30c500ea39302
David Amebley <davameb@gmail.com>
2023-04-09 22:22:29 +0300

    Add api endpoint and addStation function for adding new Station

--- backend/routes/stationRoutes.ts
+++ backend/routes/stationRoutes.ts
@@ -1,7 +1,5 @@
 // stationRoutes.ts
 import express from 'express';
-import Station from '../models/station';
-import Journey from '../models/journey';
 import { addStation, getStation, getStations } from '../controllers/stationController';
 
 const router = express.Router();
