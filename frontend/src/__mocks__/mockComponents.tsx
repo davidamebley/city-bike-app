ecd6a0fb13c92c9a0851b3dc9caa95a58c08b806
David Amebley <davameb@gmail.com>
2023-04-22 23:23:42 +0300

    Test SingleStationView Component

---
+++ frontend/src/__mocks__/mockComponents.tsx
@@ -0,0 +1,2 @@
+export const MockStationMap = () => <div data-testid="station-map" />;
+export const MockMapModal = () => <div data-testid="map-modal" />;
--- frontend/src/tests/SingleStationView.test.tsx
+++ frontend/src/tests/SingleStationView.test.tsx
@@ -1,25 +1,74 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { SingleStationView } from '../components/SingleStationView'; 
+import { SingleStationView } from '../components/SingleStationView';
+import { MockStationMap, MockMapModal } from '../__mocks__/mockComponents';
 
-jest.mock('./StationMap', () => () => <div data-testid="station-map" />);
-jest.mock('./MapModal', () => () => <div data-testid="map-modal" />);
+const mockStationData = {
+  station: {
+    _id: '1',
+    name: 'Station 1',
+    address: 'Address 1',
+    x: 12.34,
+    y: 56.78,
+    journeysStarting: 10,
+    journeysEnding: 8,
+  },
+  journeysStarting: 10,
+  journeysEnding: 8,
+  averageStartingDistance: 5000,
+  averageEndingDistance: 4500,
+  popularReturnStations: [
+    { _id: '2', name: 'Station 2', count: 4 },
+    { _id: '3', name: 'Station 3', count: 3 },
+  ],
+  popularDepartureStations: [
+    { _id: '4', name: 'Station 4', count: 5 },
+    { _id: '5', name: 'Station 5', count: 2 },
+  ],
+};
 
-jest.mock('../__mocks__/fetchStation');
+const stationId = '1';
 
-test('renders SingleStationView and its components', async () => {
-  const stationId = '1';
-  const onBack = jest.fn();
-  const { findByTestId } = render(
-    <SingleStationView stationId={stationId} onBack={onBack} />
-  );
+beforeEach(async () => {
+  jest.mock('../components/StationMap.tsx', () => MockStationMap);
+  jest.mock('../components/MapModal.tsx', () => MockMapModal);
 
-  // Check if the station name is rendered
-  expect(await screen.findByText('Station 1')).toBeInTheDocument();
+  global.fetch = jest.fn().mockImplementation(() => Promise.resolve(new Response(JSON.stringify(mockStationData))));
+  
+  await act(async () => {
+    const onBack = jest.fn();
+    render(<SingleStationView stationId={stationId} onBack={onBack} />);
+  });
+});
 
-  // Check if the StationMap component is rendered
-  expect(await findByTestId('station-map')).toBeInTheDocument();
+describe('SingleStationView Component', () => {
+  // clear all the mock functions after each test case
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-  // Check if the MapModal component is rendered
-  expect(await findByTestId('map-modal')).toBeInTheDocument();
+  it('renders a single station', async () => {
+    // Check if SingleStationView is rendered
+    await waitFor(() => expect(screen.getByTestId('single-station-view')).toBeInTheDocument());
+
+    // Check if the station name is rendered
+    await waitFor(() => expect(screen.getByText('Station 1')).toBeInTheDocument());
+  });
+
+  it('renders StationMap component', async () => {
+    // Check if the StationMap component is rendered
+    await waitFor(() => expect(screen.getByTestId('single-station-view').querySelector('.container__map')).toBeInTheDocument());
+  });
+
+  it('renders MapModal when clicking on Show Map', async () => {
+    // Check if Show Map element is rendered
+    const showMap = await waitFor(() => (screen.getByText('Show Map')));
+    await waitFor(() => expect(showMap).toBeInTheDocument());
+    // Click on the Show Map element
+    await waitFor(() => fireEvent.click(showMap));
+
+    // Check if the MapModal is rendered
+    await waitFor(() => expect(screen.getByTestId('single-station-view').querySelector('.map-modal__open')).toBeInTheDocument())
+  });
+
 });
