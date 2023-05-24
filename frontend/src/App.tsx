fb2a7cefb378a45776d73096422568eacf159c02
David Amebley <davameb@gmail.com>
2023-05-24 15:24:24 +0300

    Add a hamburger menu to app's main Tabs using Material UI's Drawer component and media queries for handling responsiveness.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { AppBar, Tabs, Tab, Toolbar, Typography, Box } from '@mui/material';
+import { AppBar, Tabs, Tab, Toolbar, Typography, Box, IconButton, Drawer, List, ListItem, useMediaQuery, useTheme, ListItemButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 import { JourneyList } from './components/JourneyList';
 import { StationList } from './components/StationList';
@@ -7,9 +8,18 @@ import './App.css'
 
 function App() {
   const [activeTab, setActiveTab] = useState(0);
+  const [open, setOpen] = useState(false);
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm')); // 'sm' = 600px.
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setActiveTab(newValue);
+    if (isMobile) setOpen(false); // Close the Drawer when a Tab is selected on mobile
+  };
+
+  const handleMenu = () => {
+    setOpen(!open);
   };
 
   return (
@@ -26,10 +36,35 @@ function App() {
               Helsinki City Bikes
             </Typography>
           </Box>
-          <Tabs value={activeTab} onChange={handleChange} textColor="primary">
-            <Tab data-testid="journeys-list-button" label="Journeys" />
-            <Tab data-testid="stations-list-button" label="Stations" />
-          </Tabs>
+          {
+            isMobile ? (
+              <>
+                <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleMenu}>
+                  <MenuIcon />
+                </IconButton>
+                <Drawer anchor="right" open={open} onClose={handleMenu}>
+                  <List>
+                    <ListItem disablePadding>
+                      <ListItemButton onClick={(event) => handleChange(event, 0)}>
+                        Journeys
+                      </ListItemButton>
+                    </ListItem>
+
+                    <ListItem disablePadding>
+                      <ListItemButton onClick={(event) => handleChange(event, 1)}>
+                        Stations
+                      </ListItemButton>
+                    </ListItem>
+                  </List>
+                </Drawer>
+              </>
+            ):(
+              <Tabs value={activeTab} onChange={handleChange} textColor="primary">
+                <Tab data-testid="journeys-list-button" label="Journeys" />
+                <Tab data-testid="stations-list-button" label="Stations" />
+              </Tabs>
+            )
+          }
         </Toolbar>
       </AppBar>
       {activeTab === 0 && <JourneyList />}
